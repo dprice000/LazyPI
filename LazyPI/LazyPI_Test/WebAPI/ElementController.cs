79eee3e69f996430f12e6d934a5c19cb8b7533fc
David Price <Davidp809@gmail.com>
2017-03-03 16:24:51 -0500

    Adding mocked Controllers for future testing.

---
+++ LazyPI/LazyPI_Test/WebAPI/ElementController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LazyPI.LazyObjects;
+using LazyPI.Common;
+
+namespace LazyPI_Test.WebAPI
+{
+    public class ElementController : IAFElementController
+    {
+        private List<AFElement> _elements = new List<AFElement>();
+        private List<AFAttribute> _attrs = new List<AFAttribute>();
+        private List<AFEventFrame> _eventframes = new List<AFEventFrame>();
+
+        public AFElement Find(Connection Connection, string ElementID)
+        {
+            var ele = _elements.Find(x => x.ID == ElementID);
+
+            return ele;
+        }
+
+        public AFElement FindByPath(Connection Connection, string Path)
+        {
+           var ele = _elements.Find(x => x.Path == Path);
+
+            return ele;
+        }
+
+        public bool Update(Connection Connection, AFElement Element)
+        {
+            var org = _elements.Find(x => x.ID == Element.ID);
+
+            int index = _elements.IndexOf(org);
+
+            _elements[index] = org;
+
+            return true;
+        }
+
+        public bool Delete(Connection Connection, string ElementID)
+        {
+            var ele = _elements.Find(x => x.ID == ElementID);
+
+            _elements.Remove(ele);
+
+            return true;
+        }
+
+        public bool CreateAttribute(Connection Connection, string ParentID, AFAttribute Attr)
+        {
+            _attrs.Add(Attr);
+
+            return true;
+        }
+
+        public bool CreateChildElement(Connection Connection, string ParentID, AFElement Element)
+        {
+            _elements.Add(Element);
+
+            return true;
+        }
+
+        public string GetElementTemplate(Connection Connection, string ElementID)
+        {
+            return "FakeTemplateName";
+        }
+
+        public IEnumerable<string> GetCategories(Connection Connection, string ElementID)
+        {
+            //TODO: we need to mock this
+            return null;
+        }
+
+        public IEnumerable<AFAttribute> GetAttributes(Connection Connection, string ElementID, string NameFilter = "*", string CategoryName = "*", string TemplateName = "*", string ValueType = "*", bool SearchFullHierarchy = false, string SortField = "Name", string SortOrder = "Ascending", int StartIndex = 0, bool ShowExcluded = false, bool ShowHidden = false, int MaxCount = 1000)
+        {
+            return _attrs;
+        }
+
+        public IEnumerable<AFElement> GetElements(Connection Connection, string RootID, string NameFilter = "*", string CategoryName = "*", string TemplateName = "*", ElementType ElementType = ElementType.Any, bool SearchFullHierarchy = false, string SortField = "Name", string SortOrder = "Ascending", int StartIndex = 0, int MaxCount = 1000)
+        {
+            return _elements;
+        }
+
+        public IEnumerable<AFEventFrame> GetEventFrames(Connection Connection, string ElementID, SearchMode SearchMode, DateTime StartTime, DateTime EndTime, string NameFilter, string CategoryName, string TemplateName, string SortField, string SortOrder, int StartIndex, int MaxCount)
+        {
+            return _eventframes;
+        }
+    }
+}
---
+++ LazyPI/LazyPI_Test/WebAPI/EventFrameController.cs
@@ -0,0 +1,89 @@
+﻿using LazyPI.Common;
+using LazyPI.LazyObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazyPI_Test.WebAPI
+{
+    public class EventFrameController : IAFEventFrameController
+    {
+        List<AFEventFrame> _eventFrames = new List<AFEventFrame>();
+        List<AFAttribute> _attrs = new List<AFAttribute>();
+
+        public AFEventFrame Find(Connection Connection, string FrameID)
+        {
+            return _eventFrames.Find(x => x.ID == FrameID);
+        }
+
+        public AFEventFrame FindByPath(Connection Connection, string Path)
+        {
+            return _eventFrames.Find(x => x.Path == Path);
+        }
+
+        public bool Update(Connection Connection, AFEventFrame EventFrame)
+        {
+            var frame = _eventFrames.Find(x => x.ID == EventFrame.ID);
+            int index = _eventFrames.IndexOf(frame);
+
+            _eventFrames[index] = frame;
+
+            return true;
+        }
+
+        public bool Delete(Connection Connection, string FrameID)
+        {
+            var ele = _eventFrames.Find(x => x.ID == FrameID);
+
+            _eventFrames.Remove(ele);
+
+            return true;
+        }
+
+        public bool CaptureValues(Connection Connection, string FrameID)
+        {
+
+        }
+
+        public bool CreateAttribute(Connection Connection, string FrameID, AFAttribute Attribute)
+        {
+            _attrs.Add(Attribute);
+
+            return true;
+        }
+
+        public bool CreateEventFrame(Connection Connection, string ParentID, AFEventFrame EventFrame)
+        {
+            _eventFrames.Add(EventFrame);
+
+            return true;
+        }
+
+        public string GetEventFrameTemplate(Connection Connection, string FrameID)
+        {
+            return "FakeEFTemplate";
+        }
+
+        public IEnumerable<AFAttribute> GetAttributes(Connection Connection, string FrameID, string NameFilter, string CategoryName, string TemplateName, string ValueType, bool SearchFullHierarchy, string SortField, string SortOrder, int StartIndex, bool ShowExcluded, bool ShowHidden, int MaxCount)
+        {
+
+        }
+
+        public IEnumerable<string> GetCategories(Connection Connection, string FrameID)
+        {
+
+        }
+
+        public IEnumerable<AFElement> GetReferencedElements(Connection Connection, string FrameID)
+        {
+
+        }
+
+        public IEnumerable<AFEventFrame> GetEventFrames(Connection Connection, string FrameID)
+        {
+
+        }
+    }
+}
