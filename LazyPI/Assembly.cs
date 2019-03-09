392521c031ecea375ea8b6bc67de4aca232a96ac
David Price <davidp809@gmail.com>
2019-03-09 10:34:15 -0500

    Adding InternalsVisible attribute.

---
+++ LazyPI/Assembly.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("LazyPI_Test")]
+namespace LazyPI
+{
+    /// <summary>
+    /// This is not an AssemblyInfo File. This was just made as a good place to add the InisVisibileTo attribute
+    /// </summary>
+    internal class Assembly
+    {
+    }
+}
